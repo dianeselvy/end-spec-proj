8845b7897ea3d8880ca87c728f73987a1225621d
dianeselvy <dianeselvy@hotmail.com>
2016-07-27 18:53:14 -0700

    Added some wireframe content

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
   <meta charset="utf-8">
   <meta name="robots" content="noindex">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
   <link href='https://fonts.googleapis.com/css?family=Raleway:400,600,700' rel='stylesheet' type='text/css'>
 </head>
 <header>
-<img src="http://estp.dianeselvy.com/wp-content/uploads/2016/07/estp-2.png">
+<a href="http://estp:8888/"><img src="http://estp.dianeselvy.com/wp-content/uploads/2016/07/estp-2.png"></a>
 
 <div id="h-buttons">
 <ul>
@@ -25,14 +25,7 @@
   <div id="content">
   <div id="page-content">
 	<div id="navigation">
-	<ul>
-		<li><a href="#">ABOUT</a></li>
-		<li><a href="#">SHOWS</a></li>
-		<li><a href="#">BLOG</a></li>
-		<li><a href="#">CALENDAR</a></li>
-		<li><a href="#">FAQ</a></li>
-		<li><a href="#">CONTACT</a></li>
-	</ul>
+	<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container' => 'div','container_id' => 'navigation', ) ); ?>
 	</div>
 	
 	<div id="banner">
@@ -41,8 +34,30 @@
 			<p>More Info >></p>
 		</div>
 	</div>
-	
-	
-  </div>
-  </div>
+
+  <div id="middle">
+	<div id="middle-left">
+		<h1>“ESTP Mission Statement not about endangered species; but about endangered plays...”</h1><br>
+		<h2>Header</h2><br>
+		<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly There are words which don't look even slightly There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly There are words which don't look even slightly.</p>
+	</div> <!-- end middle-left -->
+
+	<div id="middle-right">
+		<h2>Upcoming Shows</h2><br>
+	</div> <!--end middle-right -->
+  </div> <!-- end middle -->
+
+  </div> <!-- end page-content -->
+  </div> <!-- end content-->
 </body>
+
+
+<footer>
+<div id="f-buttons">
+<ul>
+	<li><a href="#">Buy Tickets</a></li>
+	<li><a href="#">Donate</a></li>
+	<li><a href="#">Subscribe</a></li>
+</ul>
+</div>
+</footer>

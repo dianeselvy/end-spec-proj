53a6a96f10e2e9c719595d40ca06f7f3ff34f581
dianeselvy <dianeselvy@hotmail.com>
2016-07-27 17:06:20 -0700

    updated files

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Endangered Species Theatre Project</title>
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css" />
+  <link href='https://fonts.googleapis.com/css?family=Raleway:400,600,700' rel='stylesheet' type='text/css'>
+</head>
+<header>
+<img src="http://estp.dianeselvy.com/wp-content/uploads/2016/07/estp-2.png">
+
+<div id="h-buttons">
+<ul>
+	<li><a href="#">Buy Tickets</a></li>
+	<li><a href="#">Donate</a></li>
+	<li><a href="#">Subscribe</a></li>
+</ul>
+</div>
+
+</header>
+
+<body>
+  <div id="content">
+  <div id="page-content">
+	<div id="navigation">
+	<ul>
+		<li><a href="#">ABOUT</a></li>
+		<li><a href="#">SHOWS</a></li>
+		<li><a href="#">BLOG</a></li>
+		<li><a href="#">CALENDAR</a></li>
+		<li><a href="#">FAQ</a></li>
+		<li><a href="#">CONTACT</a></li>
+	</ul>
+	</div>
+	
+	<div id="banner">
+	<img src="#">
+		<div id="banner-blip">
+			<p>More Info >></p>
+		</div>
+	</div>
+	
+	
+  </div>
+  </div>
+</body>

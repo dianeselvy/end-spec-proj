f9bb132570e6be68652affe45358b6061b5c067a
smelani <sethmelani@SETHs-MacBook-Pro.local>
2016-08-03 18:20:02 -0700

    Added new fonts, header and paragraph sizes.

--- front-page.php
+++ front-page.php
@@ -13,7 +13,7 @@
 	</div> <!--end middle-right -->
 	
 	<div id="middle-left">
-		<h1>“ESTP Mission Statement not about endangered species; but about endangered plays...”</h1><br>
+		<h1>ÒESTP Mission Statement not about endangered species; but about endangered plays...Ó</h1><br>
 		<h2>Header</h2><br>
 		<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly There are words which don't look even slightly There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly There are words which don't look even slightly.</p>
 	</div> <!-- end middle-left -->

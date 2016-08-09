e73a7d5d50f34b6b9044c9b799e63de2e5eda565
smelani <sethmelani@SETHs-MacBook-Pro.local>
2016-08-09 15:31:38 -0700

    Updated font for h1, Put in some front page content, removed all padding/margin from h2

--- front-page.php
+++ front-page.php
@@ -13,9 +13,23 @@
 	</div> <!--end middle-right -->
 	
 	<div id="middle-left">
-		<h1>“ESTP Mission Statement not about endangered species; but about endangered plays...”</h1><br>
-		<h2>Header</h2><br>
-		<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly There are words which don't look even slightly There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly There are words which don't look even slightly.</p>
+		<h1>“Lending live voice to the plays that now lie silent on our bookshelves”</h1><br>
+		<h2>Endangered Species Theatre Project</h2><br>
+		<p>Founded in 2011 by a group of esteemed Seattle directors and
+		actors, ESTP brings great plays to life in elegantly staged readings.
+		Infrequently or never before produced in Seattle, these vigorous
+		works have been neglected due to their large casts or heavy scenic
+		requirements – or having unjustly fallen out of cultural memory.</p>
+		
+		<p>ESTP each month gathers distinguished local theatre
+		artists eager to explore the genius of playwrights such as
+		Sidney Kingsley, Jean Giraudoux, Lorraine Hansberry, Arthur
+		Schnitzler, J. M. Barrie, Enid Bagnold, Bertholt Brecht,
+		George Bernard Shaw, Noël Coward, Lillian Hellman, and George
+		S. Kaufman, to name just a few.  We and our audiences are
+		constantly reminded that the great plays speak as
+		dramatically and touchingly as they ever did, showing us the
+		continuum of the human condition.</p>
 	</div> <!-- end middle-left -->
 	
   </div> <!-- end middle -->

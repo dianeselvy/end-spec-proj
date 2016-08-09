dc52d22e1cad85263fb1d4cf66c41c7ad0b730d2
Arel W <arelwatson@tests-MacBook-Pro-2.local>
2016-08-09 10:45:43 -0700

    Banner Function

--- functions.php
+++ functions.php
@@ -14,3 +14,34 @@ register_nav_menus(array(
 
 
 add_theme_support( 'post-thumbnails' ); 
+
+
+
+
+function get_banner(){
+    
+    global $post;
+    $attachments = get_children(array('post_parent'=> $post->ID, 'order' => 'ASC', 'orderby' => 'menu_order', 'post_type' => 'attachment', 'post_mime_type'=> 'image'));
+    
+    if ($attachments) {
+        
+         echo '<div class="banner">';
+    
+foreach($attachments as $attachment ){
+    echo '<div>';
+    echo '<a href="'.wp_get_attachment_image_src( $attachment_id, 'medium' ).'">';
+        echo wp_get_attachment_image($attachment->ID);
+        echo '</a>';
+        echo '</div>';
+    
+}
+    
+       
+    echo '</div>';
+       
+    }
+    
+
+    
+    
+}

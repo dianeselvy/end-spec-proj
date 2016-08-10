e900a8b93c9f0e3a68aac7e0fda75a6206261d0d
smelani <sethmelani@SETHs-MacBook-Pro.local>
2016-08-10 15:43:44 -0700

    Added images and more styling to home page

--- sidebar.php
+++ sidebar.php
@@ -1,19 +1,21 @@
-		<h2>Upcoming Shows</h2>
-		<div id="showinfo">
-			<img src="<?php bloginfo('template_directory'); ?>/images/.png" />
-			<h3>Show Title</h3>
-			<p>Show Date</p>
-			<p>$10.00</p>
-		</div><br> <!-- end showinfo -->
-		<div id="showinfo">
-			<img src="<?php bloginfo('template_directory'); ?>/images/.png" />
-			<h3>Show Title</h3>
-			<p>Show Date</p>
-			<p>$10.00</p>
-		</div><br> <!-- end showinfo -->
-		<div id="showinfo">
-			<img src="<?php bloginfo('template_directory'); ?>/images/.png" />
-			<h3>Show Title</h3>
-			<p>Show Date</p>
-			<p>$10.00</p>
-		</div> <!-- end showinfo -->
+        <div id="middle-right-content">
+            <h2>Upcoming Shows</h2>
+            <div id="showinfo">
+                <img src="<?php bloginfo('template_directory'); ?>/images/miss-lulu1-2.jpg" />
+                <h3>Show Title</h3>
+                <p>Show Date</p>
+                <p>$10.00</p>
+            </div><br> <!-- end showinfo -->
+            <div id="showinfo">
+                <img src="<?php bloginfo('template_directory'); ?>/images/parfumerie_flyer1-2.jpg" />
+                <h3>Show Title</h3>
+                <p>Show Date</p>
+                <p>$10.00</p>
+            </div><br> <!-- end showinfo -->
+            <div id="showinfo">
+                <img src="<?php bloginfo('template_directory'); ?>/images/traveling_lady_poster1-2.jpg" />
+                <h3>Show Title</h3>
+                <p>Show Date</p>
+                <p>$10.00</p>
+            </div> <!-- end showinfo -->
+        </div>

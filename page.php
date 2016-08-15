b6001a57b843cb1ce75b42080d772769185bcdb3
dianeselvy <dianeselvy@hotmail.com>
2016-08-15 16:00:19 -0700

    fixes calendar styles!

--- page.php
+++ page.php
@@ -6,20 +6,37 @@
   <div id="page-content">	
   <div id="middle">
   
+
 <?php if( is_page() && !is_page(11)):  ?> 
   	<div id="middle-right">
 	<?php get_sidebar(); ?>
 	</div> <!--end middle-right -->
 <?php endif; ?>	
 	
+
+
+<?php if( is_page() && !is_page(11)):  ?> 
 	<div id="middle-left">
+	
   	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	
     <?php the_content(''); ?>
 	
 	<?php endwhile; endif; ?>  
 	</div> <!-- end middle-left -->
-  
+<?php endif; ?>
+
+
+<?php if( is_page(11)):  ?> 
+	<div id="middle-left" style="margin-left:15%;padding-bottom:20px;">
+	
+  	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	
+    <?php the_content(''); ?>
+	
+	<?php endwhile; endif; ?>  
+	</div> <!-- end middle-left -->
+<?php endif; ?>
   
 
 	</div> <!--end middle-->

b7b377eec3448ed31ec535fdacbbe073a41c0f4f
dianeselvy <dianeselvy@hotmail.com>
2016-08-15 20:05:16 -0700

    styles blog links

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ echo do_shortcode( '[breadcrumb]' );
 <h2>Blog: Past Show Summaries</h2>
 <br>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<h2><a href="<?php the_permalink(); ?>">
+<h2 id="blog-post"><a href="<?php the_permalink(); ?>">
 <?php the_title(); ?></a></h2>
 <?php the_excerpt(); ?>
 <?php endwhile; endif; ?>
